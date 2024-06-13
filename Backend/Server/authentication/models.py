174e75e49027857c9eff0f632677e583e43859db
Mobin_Developer <special.me.force1@gmail.com>
2024-06-13 14:52:31 +0330

    One time password model added to the project otp authentication system.

--- Backend/Server/authentication/models.py
+++ Backend/Server/authentication/models.py
@@ -1,3 +1,75 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+
+
+
+class Otp(models.Model):
+    
+    class OtpStatus(models.TextChoices):
+        EXPIRED = 'EXP', 'Expired'
+        ACTIVE = 'ACT', 'Active'
+        
+    
+    status = models.CharField(
+        max_length=3,
+        choices=OtpStatus.choices,
+        default=OtpStatus.ACTIVE 
+    )
+    
+    email = models.EmailField(unique=True)
+    
+    username = models.CharField( 
+        max_length=40,
+        unique=True
+    )
+    
+    user_type = models.CharField(max_length=3)
+    
+    password = models.CharField(max_length=16)
+    
+    
+    token = models.CharField(
+        max_length=250,
+        unique=True
+    )
+    
+    code = models.CharField(max_length=6)
+    
+    expiration = models.DateTimeField()
+    
+    class Meta:
+        verbose_name = "One-Time Password"
+        verbose_name_plural = "One-Time Passwords"
+        
+    
+    def __str__(self):
+        return f'{self.status}----{self.code}----{self.token}'
+        
+        
+    def get_expiration_time(self):
+        
+        now = timezone.now()
+        
+        self.expiration = now + timezone.timedelta(minutes=1)
+        
+        return self.expiration
+    
+    def validate_status(self):
+        
+        if self.expiration >= timezone.now():
+            
+            self.status == self.OtpStatus.EXPIRED
+            
+            return self.status
+    
+    def delete_expired_one_time_passwords(self):
+        
+        if self.status == self.OtpStatus.EXPIRED:
+            
+            self.delete()
+            
+            return 'Deleted'
+        else:
+            return 'Is not expired'
