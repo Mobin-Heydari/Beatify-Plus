3648d4dc45629f22180c49e9dff8a00a08700176
Mobin_Developer <special.me.force1@gmail.com>
2024-06-17 12:46:27 +0330

    Update mathod is added to the beat view set and serializer.

--- Backend/Server/beats/serializers.py
+++ Backend/Server/beats/serializers.py
@@ -6,21 +6,29 @@ from .models import Beat, BeatInformation
 
 
 class BeatSerializer(serializers.ModelSerializer):
-    
+    # Define a serializer method field for the info field
     info = serializers.SerializerMethodField()
     
+    # Define file fields for beat_file and image
     beat_file = serializers.FileField()
     image = serializers.FileField()
     
     class Meta:
+        # Specify the model and fields for the serializer
         model = Beat
         fields = '__all__'
         
+        
+    def get_info(self, obj):
+        # Return the serialized BeatInformation instance
+        serializer = BeatInformationSerializer(instance=obj.Beat_Info)
+        return serializer.data    
     
     def create(self, validated_data):
-        
+        # Get the request object from the context
         request = self.context.get('request')
         
+        # Create a new Beat instance
         beat = Beat.objects.create(
             title = validated_data['title'],
             owner = request.user,
@@ -30,25 +38,47 @@ class BeatSerializer(serializers.ModelSerializer):
             image = validated_data['image']
         )
         
+        # Save the Beat instance
         beat.save()
         
+        # Create a new BeatInformation instance
         beat_info = BeatInformation.objects.create(
             beat = beat
         )
         
+        # Save the BeatInformation instance
         beat_info.save()
         
         return beat
     
-    def get_info(self, obj):
-        serializer = BeatInformationSerializer(instance=obj.Beat_Info)
-        return serializer.data
     
+    def update(self, instance, validated_data):
+        # Update the title and description fields
+        if 'title' in validated_data:
+            instance.title = validated_data['title']
+        if 'description' in validated_data:
+            instance.description = validated_data['description']
+        if 'title' in validated_data:
+            instance.slug = f'{instance.owner.username}-{validated_data['title']}'
+        instance.save()
+        
+        # Handle file updates
+        if 'beat_file' in validated_data:
+            # Update the beat_file field
+            instance.beat_file = validated_data['beat_file']
+            
+        # Handle file updates
+        if 'image' in validated_data:
+            # Update the image field
+            instance.image = validated_data['image']
+            
+        instance.save()
+        
+        return instance
     
 
 class BeatInformationSerializer(serializers.ModelSerializer):
-    
+    # Specify the model and fields for the serializer
     class Meta:
         model = BeatInformation
         fields = "__all__"
-    
--- Backend/Server/beats/views.py
+++ Backend/Server/beats/views.py
@@ -7,47 +7,138 @@ from rest_framework import status
 from . import serializers
 from .models import Beat, BeatInformation
 from .paginations import BeatPagination
+from .permissions import IsOwnerOrReadOnly
 
 
 
 
 
 class BeatViewSet(ViewSet, BeatPagination):
-    
+    """
+    ViewSet for Beats
+    """
+    # Define the permission classes for this viewset
+    permission_classes = [IsOwnerOrReadOnly]
+
     def list(self, request):
+        """
+        List all beats
+        """
+        # Get all beats and order them by creation date (newest first)
         queryset = Beat.objects.all().order_by('-created')
-        result = self.paginate_queryset(queryset, request)
-        serializer = serializers.BeatSerializer(result, many=True)
-        return self.get_paginated_response(serializer.data)
         
-    
+        # Paginate the queryset using the BeatPagination class
+        result = self.paginate_queryset(queryset, request)
+        
+        # Serialize the paginated result using the BeatSerializer
+        serializer = serializers.BeatSerializer(result, many=True)
+        
+        # Return the paginated response
+        return self.get_paginated_response(serializer.data)
+
+
     def retrieve(self, request, pk):
+        """
+        Retrieve a single beat by slug (pk)
+        """
+        # Get the beat instance by slug (pk)
         query = get_object_or_404(Beat, slug=pk)
+        
+        # Check if the beat is published or the requesting user is the owner
         if query.published_status == 'Published' or query.owner == request.user:
+            # Serialize the beat instance using the BeatSerializer
             serializer = serializers.BeatSerializer(instance=query)
+            
+            # Return the serialized data with a 200 OK status
             return Response(serializer.data, status=status.HTTP_200_OK)
         else:
+            # Return a 403 Forbidden response if the beat is not published or the user doesn't have access
             return Response(
                 {
                     'Detail' : 'The beat is not published or you dont have the access to read'
                 },
                 status=status.HTTP_403_FORBIDDEN
-            )       
-    
+            )
+
+
     def create(self, request):
-        if request.user.is_authenticated == True:
+        """
+        Create a new beat
+        """
+        # Check if the requesting user is authenticated
+        if request.user.is_authenticated:
             user = request.user
-            # Then we will check that this user is the Producer user or Musician user or not?
+            
+            # Check if the user is a Producer or Musician type
             if user.user_type == 'PRD':
-                # If yes then we will go forward
+                # Create a new beat instance using the BeatSerializer
                 serializer = serializers.BeatSerializer(data=request.data, context={'request': request})
-                # If the data is valid then save the data into database else give errors
+                
+                # If the data is valid, save the instance and return a 201 Created response
                 if serializer.is_valid(raise_exception=True):
                     serializer.save()
-                    return Response({"Response":"Created."}, status=status.HTTP_201_CREATED)
+                    return Response(
+                        {
+                            'Detail':'Created.'
+                        },
+                         status=status.HTTP_201_CREATED
+                    )
                 else:
+                    # Return a 400 Bad Request response if the data is invalid
                     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
             else:
-                return Response({"error":"invalid user type."}, status=status.HTTP_400_BAD_REQUEST)
+                # Return a 400 Bad Request response if the user type is invalid
+                return Response(
+                    {
+                        'Detail':'invalid user type.'
+                    },
+                    status=status.HTTP_400_BAD_REQUEST
+                )
         else:
-            return Response({"error":"Unauthorized user."},status=status.HTTP_401_UNAUTHORIZED)
+            # Return a 401 Unauthorized response if the user is not authenticated
+            return Response(
+                {
+                    'Detail':'Unauthorized user.'
+                },
+                status=status.HTTP_401_UNAUTHORIZED
+            )
+
+
+    def update(self, request, pk):
+        """
+        Update a beat
+        """
+        # Get the beat instance by slug (pk)
+        queryset = get_object_or_404(Beat, slug=pk)
+        
+        # Check if the requesting user is authenticated
+        if request.user.is_authenticated:
+            # Check object permissions using the permission classes
+            self.check_object_permissions(request, queryset)
+            
+            # Update the beat instance using the BeatSerializer
+            serializer = serializers.BeatSerializer(queryset, data=request.data, partial=True)
+            
+            # If the data is valid, update the instance and return a 200 OK response
+            if serializer.is_valid(raise_exception=True):
+                
+                serializer.update(instance=queryset, validated_data=serializer.validated_data)
+                
+                return Response(
+                    serializer.data,
+                    status=status.HTTP_200_OK
+                )
+            else:
+                # Return a 400 Bad Request response if the data is invalid
+                return Response(
+                    serializer.errors,
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+        else:
+            # Return a 401 Unauthorized response if the user is not authenticated
+            return Response(
+                {
+                    'Detail':'you are not authenticated.'
+                },
+                status=status.HTTP_401_UNAUTHORIZED
+            )
