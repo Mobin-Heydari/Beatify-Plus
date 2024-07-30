805a053c82a208f47393fec362981fd7cebb429e
Mobin_Developer <special.me.force1@gmail.com>
2024-07-30 14:50:40 +0330

    navbar changed

--- Frontend/components/ui/navbar.tsx
+++ Frontend/components/ui/navbar.tsx
@@ -1,16 +1,18 @@
 import React from "react";
 
 import ProfileDropDown from "./profileDropDown";
-import SearchBar from "./searchbar";
 import CartDropDown from "./cart-dropdown";
+import SearchBar from "./searchbar";
+import Drawer from "./drawer";
 
 
 export default function Navbar() {
     return (
         <nav className="navbar">
-            <SearchBar></SearchBar>
+            <Drawer />
+            <SearchBar />
             <div className="dorp-down-nav-contents">
-                <CartDropDown/>
+                <CartDropDown />
                 <ProfileDropDown />   
             </div>
         </nav>
--- Frontend/components/ui/searchbar.tsx
+++ Frontend/components/ui/searchbar.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 export default function SearchBar() {
     return (
-        <label className="input search-bar">
+        <label className="input search-bar hidden lg:flex">
             <img src="assets/images/icons/search.png" alt="search" />
             <input type="text" className="grow" placeholder="سرچ" />
         </label>
